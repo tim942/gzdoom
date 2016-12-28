681740ed6753507b9c6e544f63e0a2f7dd563a5a
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-12-28 16:08:32 +0200

    gl_scale_viewport CVAR is now saved to config file
    See https://forum.drdteam.org/viewtopic.php?t=7339

--- src/gl/renderer/gl_renderer.cpp
+++ src/gl/renderer/gl_renderer.cpp
@@ -70,7 +70,7 @@
 
 EXTERN_CVAR(Int, screenblocks)
 
-CVAR(Bool, gl_scale_viewport, true, 0);
+CVAR(Bool, gl_scale_viewport, true, CVAR_ARCHIVE);
 
 //===========================================================================
 // 

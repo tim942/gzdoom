a17467a33824e6045d31ee2918f83d465add9c72
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-02-05 11:51:19 +0100

    - allow localization of Hexen's original ACS strings.
    This way of looking up strings is intentionally limited to content from Hexen.wad and Hexdd.wad.
    
    # Conflicts:
    #	src/maploader/maploader.cpp
    #	src/p_acs.cpp
    #	src/p_acs.h

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -2004,7 +2004,7 @@ void FBehavior::StaticLoadDefaultModules ()
 	}
 }
 
-FBehavior *FBehavior::StaticLoadModule (int lumpnum, FileReader *fr, int len)
+FBehavior *FBehavior::StaticLoadModule (int lumpnum, FileReader *fr, int len, int reallumpnum)
 {
 	if (lumpnum == -1 && fr == NULL) return NULL;
 
@@ -2017,7 +2017,7 @@ FBehavior *FBehavior::StaticLoadModule (int lumpnum, FileReader *fr, int len)
 	}
 
 	FBehavior * behavior = new FBehavior ();
-	if (behavior->Init(lumpnum, fr, len))
+	if (behavior->Init(lumpnum, fr, len, reallumpnum))
 	{
 		return behavior;
 	}
@@ -2280,7 +2280,7 @@ FBehavior::FBehavior()
 }
 	
 	
-bool FBehavior::Init(int lumpnum, FileReader * fr, int len)
+bool FBehavior::Init(int lumpnum, FileReader * fr, int len, int reallumpnum)
 {
 	uint8_t *object;
 	int i;
@@ -2373,6 +2373,8 @@ bool FBehavior::Init(int lumpnum, FileReader * fr, int len)
 			// Forget about the compatibility cruft at the end of the lump
 			DataSize = LittleLong(((uint32_t *)object)[1]) - 8;
 		}
+
+		ShouldLocalize = false;
 	}
 	else
 	{
@@ -2386,6 +2388,17 @@ bool FBehavior::Init(int lumpnum, FileReader * fr, int len)
 		StringTable = LittleLong(((uint32_t *)Data)[1]);
 		StringTable += LittleLong(((uint32_t *)(Data + StringTable))[0]) * 12 + 4;
 		UnescapeStringTable(Data + StringTable, Data, false);
+		// If this is an original Hexen BEHAVIOR, set up some localization info for it. Original Hexen BEHAVIORs are always in the old format.
+		if ((level.flags2 & LEVEL2_HEXENHACK) && gameinfo.gametype == GAME_Hexen && lumpnum == -1 && reallumpnum > 0)
+		{
+			int fileno = Wads.GetLumpFile(reallumpnum);
+			const char * filename = Wads.GetWadName(fileno);
+			if (!stricmp(filename, "HEXEN.WAD") || !stricmp(filename, "HEXDD.WAD"))
+			{
+				ShouldLocalize = true;
+			}
+		}
+
 	}
 	else
 	{
@@ -3263,7 +3276,7 @@ uint8_t *FBehavior::NextChunk (uint8_t *chunk) const
 	return NULL;
 }
 
-const char *FBehavior::StaticLookupString (uint32_t index)
+const char *FBehavior::StaticLookupString (uint32_t index, bool forprint)
 {
 	uint32_t lib = index >> LIBRARYID_SHIFT;
 
@@ -3275,10 +3288,10 @@ const char *FBehavior::StaticLookupString (uint32_t index)
 	{
 		return NULL;
 	}
-	return StaticModules[lib]->LookupString (index & 0xffff);
+	return StaticModules[lib]->LookupString (index & 0xffff, forprint);
 }
 
-const char *FBehavior::LookupString (uint32_t index) const
+const char *FBehavior::LookupString (uint32_t index, bool forprint) const
 {
 	if (StringTable == 0)
 	{
@@ -3290,7 +3303,26 @@ const char *FBehavior::LookupString (uint32_t index) const
 
 		if (index >= list[0])
 			return NULL;	// Out of range for this list;
-		return (const char *)(Data + list[1+index]);
+
+		const char *s = (const char *)(Data + list[1 + index]);
+		// Allow translations for Hexen's original strings.
+		// This synthesizes a string label and looks it up.
+		// It will only do this for original Hexen maps and PCD_PRINTSTRING operations.
+		// For localizing user content better solutions exist so this hack won't be available as an editing feature.
+		if (ShouldLocalize && forprint)
+		{
+			FString token = s;
+			token.ToUpper();
+			token.ReplaceChars(".,-+!?", ' ');
+			token.Substitute(" ", "");
+			token.Truncate(5);
+
+			FStringf label("TXT_ACS_%s_%d_%.5s", level.MapName.GetChars(), index, token);
+			auto p = GStrings[label];
+			if (p) return p;
+		}
+
+		return s;
 	}
 	else
 	{
@@ -8462,7 +8494,7 @@ scriptwait:
 
 		case PCD_PRINTSTRING:
 		case PCD_PRINTLOCALIZED:
-			lookup = FBehavior::StaticLookupString (STACK(1));
+			lookup = FBehavior::StaticLookupString (STACK(1), true);
 			if (pcd == PCD_PRINTLOCALIZED)
 			{
 				lookup = GStrings(lookup);

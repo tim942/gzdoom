1ef72619da7e17207be746ddb3c60a08df8c541e
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-02-20 19:30:39 +0100

    - initialize the language right after reading the strings.

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -2031,7 +2031,6 @@ static void D_DoomInit()
 
 	if (!batchrun) Printf ("M_LoadDefaults: Load system defaults.\n");
 	M_LoadDefaults ();			// load before initing other systems
-	SetLanguageIDs ();
 }
 
 //==========================================================================

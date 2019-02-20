fb89a01699a952c8556e0206f4bd5b43acf0e59b
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-02-20 19:04:34 +0100

    - fixed reading of exit text replacements and display of intermission text screens for Unicode.

--- src/intermission/intermission.h
+++ src/intermission/intermission.h
@@ -206,7 +206,7 @@ class DIntermissionScreenText : public DIntermissionScreen
 {
 	DECLARE_CLASS (DIntermissionScreenText, DIntermissionScreen)
 
-	const char *mText;
+	FString mText;
 	int mTextSpeed;
 	int mTextX, mTextY;
 	int mTextCounter;

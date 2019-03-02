221a947ea8bdcca60ea500f25c91988a7b949540
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-03-02 21:09:45 +0100

    - reject all DMX sounds shorter than or equal 8 bytes.
    8 bytes is the minimum header size for DMX, so for one byte of sample data it has to be 9 bytes.
    This was causing access to invalid memory when trying to read the header of something too short.
    For other file formats this is of no concern because none has a header this short.

--- src/s_sound.cpp
+++ src/s_sound.cpp
@@ -1491,7 +1491,7 @@ sfxinfo_t *S_LoadSound(sfxinfo_t *sfx, FSoundLoadBuffer *pBuffer)
 		DPrintf(DMSG_NOTIFY, "Loading sound \"%s\" (%td)\n", sfx->name.GetChars(), sfx - &S_sfx[0]);
 
 		int size = Wads.LumpLength(sfx->lumpnum);
-		if (size > 0)
+		if (size > 8)
 		{
 			auto wlump = Wads.OpenLumpReader(sfx->lumpnum);
 			auto sfxdata = wlump.Read(size);
@@ -1557,7 +1557,7 @@ static void S_LoadSound3D(sfxinfo_t *sfx, FSoundLoadBuffer *pBuffer)
 	else
 	{
 		int size = Wads.LumpLength(sfx->lumpnum);
-		if (size <= 0) return;
+		if (size <= 8) return;
 
 		auto wlump = Wads.OpenLumpReader(sfx->lumpnum);
 		auto sfxdata = wlump.Read(size);

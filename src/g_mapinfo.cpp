3b36606f8f132142768e0a53ac5e6142c75cd70b
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-03-05 00:22:26 +0100

    - process escape sequences for episode and skill names
    For these, colorization is a desirable feature.

--- src/g_mapinfo.cpp
+++ src/g_mapinfo.cpp
@@ -2018,7 +2018,7 @@ void FMapInfoParser::ParseEpisodeInfo ()
 		{
 			ParseAssign();
 			sc.MustGetString ();
-			name = sc.String;
+			name = strbin1(sc.String);
 		}
 		else if (sc.Compare ("picname"))
 		{

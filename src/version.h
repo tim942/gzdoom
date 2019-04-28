5cb86266ebf3b6518fff6362b76b4172938195a6
drfrag <drfrag666@hotmail.com>
2019-04-28 13:36:45 +0200

    - This is version 3.8.0.

--- src/version.h
+++ src/version.h
@@ -41,11 +41,11 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "g3.7.2_legacy"
+#define VERSIONSTR "g3.8.0_legacy"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 3,7,2,0
-#define RC_PRODUCTVERSION 3,7,2,0
+#define RC_FILEVERSION 3,8,0,0
+#define RC_PRODUCTVERSION 3,8,0,0
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4

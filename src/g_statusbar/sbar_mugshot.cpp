036a184a64ac145fa2357d01eae8a05eae18dad4
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-08-09 09:58:40 +0200

    - fixed handling of the "ouch" face.
    This depended on order of execution, taking the health values to compare from variables which were not synchronized properly.
    Now both the last and current health being used here are being retrieved in the same place so that further changes cannot break this again.
    
    But here it worked. (drfrag)

--- src/g_statusbar/sbar_mugshot.cpp
+++ src/g_statusbar/sbar_mugshot.cpp
@@ -231,7 +231,7 @@ FMugShot::FMugShot()
 
 void FMugShot::Reset()
 {
-	FaceHealth = -1;
+	FaceHealthNow = FaceHealthLast = -1;
 	bEvilGrin = false;
 	bNormal = true;
 	bDamageFaceActive = false;
@@ -272,7 +272,8 @@ void FMugShot::Tick(player_t *player)
 	{
 		RampageTimer = 0;
 	}
-	FaceHealth = player->health;
+	FaceHealthLast = FaceHealthNow;
+	FaceHealthNow = player->health;
 }
 
 //===========================================================================
@@ -338,7 +339,7 @@ int FMugShot::UpdateState(player_t *player, StateFlags stateflags)
 {
 	FString		full_state_name;
 
-	if (player->health > 0)
+	if (FaceHealthNow > 0)
 	{
 		if (bEvilGrin && !(stateflags & DISABLEGRIN))
 		{
@@ -350,10 +351,10 @@ int FMugShot::UpdateState(player_t *player, StateFlags stateflags)
 		}
 		bEvilGrin = false;
 
-		bool ouch = (!st_oldouch && FaceHealth - player->health > ST_MUCHPAIN) || (st_oldouch && player->health - FaceHealth > ST_MUCHPAIN);
+		bool ouch = (!st_oldouch && FaceHealthLast - FaceHealthNow > ST_MUCHPAIN) || (st_oldouch && FaceHealthNow - FaceHealthLast > ST_MUCHPAIN);
 		if (player->damagecount && 
 			// Now go in if pain is disabled but we think ouch will be shown (and ouch is not disabled!)
-			(!(stateflags & DISABLEPAIN) || (((FaceHealth != -1 && ouch) || bOuchActive) && !(stateflags & DISABLEOUCH))))
+			(!(stateflags & DISABLEPAIN) || (((FaceHealthLast != -1 && ouch) || bOuchActive) && !(stateflags & DISABLEOUCH))))
 		{
 			int damage_angle = 1;
 			if (player->attacker && player->attacker != player->mo)
@@ -374,7 +375,7 @@ int FMugShot::UpdateState(player_t *player, StateFlags stateflags)
 				}
 			}
 			bool use_ouch = false;
-			if (((FaceHealth != -1 && ouch) || bOuchActive) && !(stateflags & DISABLEOUCH))
+			if (((FaceHealthLast != -1 && ouch) || bOuchActive) && !(stateflags & DISABLEOUCH))
 			{
 				use_ouch = true;
 				full_state_name = "ouch.";
@@ -401,7 +402,7 @@ int FMugShot::UpdateState(player_t *player, StateFlags stateflags)
 			else
 			{
 				bool use_ouch = false;
-				if (((FaceHealth != -1 && ouch) || bOuchActive) && !(stateflags & DISABLEOUCH))
+				if (((FaceHealthLast != -1 && ouch) || bOuchActive) && !(stateflags & DISABLEOUCH))
 				{
 					use_ouch = true;
 					full_state_name = "ouch.";

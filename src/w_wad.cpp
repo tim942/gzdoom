600f38fd8b847b67392e372ec3d28759fd43b890
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-11-25 17:37:56 +0200

    - exported several Wads.GetLump...() methods to ZScript
    * GetLumpName() returns 8-characters lump name
    * GetLumpFullName() returns full name with path and extension
    * GetLumpNamespace() returns lump's namespace
    * GetNumLumps() returns total number of lumps
    
    https://forum.zdoom.org/viewtopic.php?t=66285

--- src/w_wad.cpp
+++ src/w_wad.cpp
@@ -371,6 +371,12 @@ int FWadCollection::GetNumLumps () const
 	return NumLumps;
 }
 
+DEFINE_ACTION_FUNCTION(_Wads, GetNumLumps)
+{
+	PARAM_PROLOGUE;
+	ACTION_RETURN_INT(Wads.GetNumLumps());
+}
+
 //==========================================================================
 //
 // GetNumFiles
@@ -1222,6 +1228,15 @@ void FWadCollection::GetLumpName(FString &to, int lump) const
 	}
 }
 
+DEFINE_ACTION_FUNCTION(_Wads, GetLumpName)
+{
+	PARAM_PROLOGUE;
+	PARAM_INT(lump);
+	FString lumpname;
+	Wads.GetLumpName(lumpname, lump);
+	ACTION_RETURN_STRING(lumpname);
+}
+
 //==========================================================================
 //
 // FWadCollection :: GetLumpFullName
@@ -1238,6 +1253,13 @@ const char *FWadCollection::GetLumpFullName (int lump) const
 		return LumpInfo[lump].lump->FullName;
 	else
 		return LumpInfo[lump].lump->Name;
+}
+
+DEFINE_ACTION_FUNCTION(_Wads, GetLumpFullName)
+{
+	PARAM_PROLOGUE;
+	PARAM_INT(lump);
+	ACTION_RETURN_STRING(Wads.GetLumpFullName(lump));
 }
 
 //==========================================================================
@@ -1271,6 +1293,13 @@ int FWadCollection::GetLumpNamespace (int lump) const
 		return ns_global;
 	else
 		return LumpInfo[lump].lump->Namespace;
+}
+
+DEFINE_ACTION_FUNCTION(_Wads, GetLumpNamespace)
+{
+	PARAM_PROLOGUE;
+	PARAM_INT(lump);
+	ACTION_RETURN_INT(Wads.GetLumpNamespace(lump));
 }
 
 //==========================================================================

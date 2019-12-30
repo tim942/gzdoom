99406558dc5202e446f36b6936637c3e2ab253fd
Alexander Kromm <mmaulwurff@gmail.com>
2019-12-30 20:26:40 +0700

    fix crash with saving null Dictionary

--- src/serializer.cpp
+++ src/serializer.cpp
@@ -2122,6 +2122,11 @@ FString DictionaryToString(const Dictionary &dict)
 
 Dictionary *DictionaryFromString(const FString &string)
 {
+	if (string.IsEmpty())
+	{
+		return nullptr;
+	}
+
 	Dictionary *const dict = new Dictionary;
 
 	rapidjson::Document doc;
@@ -2151,7 +2156,7 @@ template<> FSerializer &Serialize(FSerializer &arc, const char *key, Dictionary 
 {
 	if (arc.isWriting())
 	{
-		FString contents { DictionaryToString(*dict) };
+		FString contents { dict ? DictionaryToString(*dict) : "" };
 		return arc(key, contents);
 	}
 	else

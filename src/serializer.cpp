8df99029d185cb516abc968719463d09efbbf54d
Alexander Kromm <mmaulwurff@gmail.com>
2020-01-30 00:10:07 +0700

    fix Dictionary and DictionaryIterator memory leaks

--- src/serializer.cpp
+++ src/serializer.cpp
@@ -2127,7 +2127,7 @@ Dictionary *DictionaryFromString(const FString &string)
 		return nullptr;
 	}
 
-	Dictionary *const dict = new Dictionary;
+	Dictionary *const dict = Create<Dictionary>();
 
 	if (string.IsEmpty())
 	{

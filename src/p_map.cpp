03c97377ece0b3707caf2f67045bbedc8f1b1540
makise-homura <akemi_homura@kurisa.ch>
2020-08-20 23:55:00 +0300

    Add species parameter to A_RadiusThrust
    This is the last parameter for A_RadiusThrust() DECORATE function.
    If it is omitted or none, then A_RadiusThrust will behave as usual.
    
    If it is set for some species name, it witt thrust only that species.
    Of course, these species should be +VULNERABLE to be thrustable.

--- src/p_map.cpp
+++ src/p_map.cpp
@@ -5881,7 +5881,7 @@ int P_GetRadiusDamage(AActor *self, AActor *thing, int damage, int distance, int
 //==========================================================================
 
 int P_RadiusAttack(AActor *bombspot, AActor *bombsource, int bombdamage, int bombdistance, FName bombmod,
-	int flags, int fulldamagedistance)
+	int flags, int fulldamagedistance, FName species)
 {
 	if (bombdistance <= 0)
 		return 0;
@@ -5928,6 +5928,11 @@ int P_RadiusAttack(AActor *bombspot, AActor *bombsource, int bombdamage, int bom
 			((bombsource->flags6 & MF6_DONTHARMSPECIES) && (thing->GetSpecies() == bombsource->GetSpecies()))
 			)
 			)	continue;
+
+		if((species != NAME_None) && (thing->Species != species))
+		{
+			continue;
+		}
 
 		targets.Push(thing);
 	}

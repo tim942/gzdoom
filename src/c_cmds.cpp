3210f55e186bd6e4ed1af5321b8aa805a7271699
AFADoomer <TJLittle123@gmail.com>
2020-09-10 22:23:26 -0500

    Add string table lookup to SECRETS lump handling
    - If the hint text portion of a STRINGS lump entry begins with a '$', the value is treated as a string table lookup
    
    Allows SECRETS strings to be translated via LANGUAGE lump.  Reference: https://forum.zdoom.org/viewtopic.php?f=15&t=69827
    
    # Conflicts:
    #	src/c_cmds.cpp

--- src/c_cmds.cpp
+++ src/c_cmds.cpp
@@ -1324,7 +1324,7 @@ static void PrintSecretString(const char *string, bool thislevel)
 				else colstr = TEXTCOLOR_GREEN;
 			}
 		}
-		auto brok = V_BreakLines(ConFont, screen->GetWidth()*95/100, string);
+		auto brok = V_BreakLines(ConFont, screen->GetWidth()*95/100, *string == '$' ? GStrings(++string) : string);
 
 		for (auto &line : brok)
 		{

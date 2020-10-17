015583c620f858df3b8d1cf334015c33777bb86a
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-10-17 12:28:22 +0200

    fixed side_t::SetSpecialColor.
    This never set the needed flags to make the color appear.

--- src/scripting/vmthunks.cpp
+++ src/scripting/vmthunks.cpp
@@ -1677,11 +1677,11 @@ DEFINE_ACTION_FUNCTION_NATIVE(_Sector, RemoveForceField, RemoveForceField)
 	 ACTION_RETURN_POINTER(self->V2());
  }
 
- static void SetSideSpecialColor(side_t *self, int tier, int position, int color)
+ static void SetSideSpecialColor(side_t *self, int tier, int position, int color, int useown)
  {
 	 if (tier >= 0 && tier < 3 && position >= 0 && position < 2)
 	 {
-		 self->SetSpecialColor(tier, position, color);
+		 self->SetSpecialColor(tier, position, color, useown);
 	 }
  }
 
@@ -1691,7 +1691,8 @@ DEFINE_ACTION_FUNCTION_NATIVE(_Sector, RemoveForceField, RemoveForceField)
 	 PARAM_INT(tier);
 	 PARAM_INT(position);
 	 PARAM_COLOR(color);
-	 SetSideSpecialColor(self, tier, position, color);
+	 PARAM_BOOL(useown)
+	 SetSideSpecialColor(self, tier, position, color, useown);
 	 return 0;
  }
 

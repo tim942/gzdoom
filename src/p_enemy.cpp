47e799cb8e7016d7959494f9d9ec94975bc1c94d
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-10-24 21:19:52 +0200

    - only return at the end of the MAP07SPECIAL block.
    # Conflicts:
    #	src/p_enemy.cpp

--- src/p_enemy.cpp
+++ src/p_enemy.cpp
@@ -3144,14 +3144,13 @@ void A_BossDeath(AActor *self)
 		if (type == NAME_Fatso || samereplacement)
 		{
 			EV_DoFloor (DFloor::floorLowerToLowest, NULL, 666, 1., 0, -1, 0, false);
-			return;
 		}
 		
 		if (type == NAME_Arachnotron || samereplacement)
 		{
 			EV_DoFloor (DFloor::floorRaiseByTexture, NULL, 667, 1., 0, -1, 0, false);
-			return;
 		}
+		return;
 	}
 	else
 	{
